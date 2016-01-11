50fa95609854b1276d23a4af8d14a6bba71069ec
tuloski <michele.furci@studio.unibo.it>
2016-01-11 15:55:41 +0100

    Added qos in launch script

--- scripts/launch_global.sh
+++ scripts/launch_global.sh
@@ -5,9 +5,9 @@ export ROS_WORKSPACE=/home/odroid/catkin_ws
 export DISPLAY=:0
 sleep 20
 roslaunch mavros uniboAMSLUSB.launch &
-sleep 10
+sleep 8
 rosrun usb_cam usb_cam_node &
-sleep 10
+sleep 8
 rosrun camera_handler_sherpa camera_handler_sherpa &
 sleep 6
 rosrun web_video_server web_video_server &
@@ -18,3 +18,5 @@ roslaunch mavros unibo_GCS.launch &
 sleep 10
 roslaunch mms mms.launch &
 sleep 8
+roslaunch roslaunch qos_sensors_autopilot node.launch &
+sleep 5
