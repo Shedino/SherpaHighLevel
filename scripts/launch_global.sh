e92669b57ffe72d63f6a7ee7efe00a444385b75e
tuloski <michele.furci@studio.unibo.it>
2016-04-07 15:00:17 +0200

    Added screen scripts. Modified launch_global.sh to not launch anything because of screen.

--- scripts/launch_global.sh
+++ scripts/launch_global.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-source /opt/ros/indigo/setup.bash
-source /home/odroid/catkin_ws/devel/setup.bash
-export ROS_WORKSPACE=/home/odroid/catkin_ws
-export DISPLAY=:0
-sleep 20
-roslaunch mavros uniboAMSLUSB.launch &
-sleep 8
-rosrun usb_cam usb_cam_node &
-sleep 8
-rosrun camera_handler_sherpa camera_handler_sherpa &
-sleep 6
-rosrun web_video_server web_video_server &
-sleep 5
-rosrun mavros mavsys rate --all 10 &
-sleep 4
-roslaunch mavros unibo_GCS.launch &
-sleep 10
-roslaunch mms mms.launch &
-sleep 8
-roslaunch roslaunch qos_sensors_autopilot node.launch &
-sleep 5
+#source /opt/ros/indigo/setup.bash
+#source /home/odroid/catkin_ws/devel/setup.bash
+#export ROS_WORKSPACE=/home/odroid/catkin_ws
+#export DISPLAY=:0
+#sleep 20
+#roslaunch mavros uniboAMSLUSB.launch &
+#sleep 8
+#rosrun usb_cam usb_cam_node &
+#sleep 8
+#rosrun camera_handler_sherpa camera_handler_sherpa &
+#sleep 6
+#rosrun web_video_server web_video_server &
+#sleep 5
+#rosrun mavros mavsys rate --all 10 &
+#sleep 4
+#roslaunch mavros unibo_GCS.launch &
+#sleep 10
+#roslaunch mms mms.launch &
+#sleep 8
+#roslaunch roslaunch qos_sensors_autopilot node.launch &
+#sleep 5
