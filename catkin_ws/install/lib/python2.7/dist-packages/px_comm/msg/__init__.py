f05366e4447228ccc969f0ea72bf927d187471f2
tuloski <michele.furci@studio.unibo.it>
2016-06-07 11:40:10 +0200

    Removed (Useless) Install folder in catkin_ws

--- catkin_ws/install/lib/python2.7/dist-packages/px_comm/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._Mavlink import *
-from ._CameraInfo import *
-from ._OpticalFlow import *
--- catkin_ws/install/share/common-lisp/ros/px_comm/srv/_package_SetCameraInfo.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package px_comm-srv)
-(cl:export '(CAMERA_INFO-VAL
-          CAMERA_INFO
-          SUCCESS-VAL
-          SUCCESS
-          STATUS_MESSAGE-VAL
-          STATUS_MESSAGE
-))
